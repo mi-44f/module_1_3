8fa2968b7a9ae060f9125c8289391410d2065823
mi-44f <mifil44f@gmail.com>
2024-11-18 01:01:51 +0300

    module_1_3

---
+++ main.py
@@ -0,0 +1,17 @@
+# This is a sample Python script.
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+Chislo1 = 777
+Stroka1 = 'kakoito tekst'
+pravda = True
+flot = 5.55
+name = 'Mihail'
+print (name)
+age = 62
+print (age)
+age = age + 62
+print (age)
+is_student = True
+print (is_student)
+
